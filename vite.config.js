14ce86e74cecca19d51ef552e1a3e834e2191540
MoeezAhmed1609 <MoeezAhmed1609>
2023-08-08 11:53:59 +0500

    Deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'public/index.html'),
+      name: 'index',
+      // the proper extensions will be added
+      fileName: 'index.html',
+    },
+  },
   plugins: [react()],
 })
