544d9725ea92ee6a5aaa544be1fef68fb7245aae
MoeezAhmed1609 <MoeezAhmed1609>
2023-08-09 15:12:25 +0500

    again deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'index.html'),
-      name: 'index',
-      // the proper extensions will be added
-      fileName: 'index.html',
-    },
-  },
+  // build: {
+  //   lib: {
+  //     // Could also be a dictionary or array of multiple entry points
+  //     entry: resolve(__dirname, 'index.html'),
+  //     name: 'index',
+  //     // the proper extensions will be added
+  //     fileName: 'index.html',
+  //   },
+  // },
   plugins: [react()],
 })
